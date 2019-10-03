ec978152a3e0fa57edd36d52a63c99a00f2812f0
github username <github email address>
2019-10-03 13:48:03 -0400

    Done.

--- index.js
+++ index.js
@@ -4,11 +4,25 @@ class Formatter {
   }
 
   static sanitize(string){
-    return string.replace(/[^A-Za-z0-9-']+/g, '');
+    return string.replace(/[^A-Za-z0-9 '-]+/g, '');
   }
 
   static titleize(string){
-    return string.charAt(0).toUpperCase() + string.slice(1);
+    let exceptions = [ 'the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from' ]
+    let result = [];
+    let arrayOfWords = string.split( " " )
+    for ( let n = 0; n < arrayOfWords.length; n++ ) {
+      if ( n == 0 ) {
+        result.push( this.capitalize( arrayOfWords[ n ] ) )
+      } else {
+        if ( exceptions.includes( arrayOfWords[ n ] ) ) {
+          result.push( arrayOfWords[ n ] )
+        } else {
+          result.push( this.capitalize( arrayOfWords[ n ] ) )
+        }
+      }
 
+    }
+    return result.join( " " );
   }
 }
